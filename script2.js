5cfd12e78a9bbb027268976b31e2e8f10ac2f99b
atulgupta0403 <atulgupta0403@gmail.com>
2024-09-25 12:15:04 +0530

    login completed

--- script2.js
+++ script2.js
@@ -1,7 +1,17 @@
 const down = document.querySelector(".down");
 const login = document.querySelector(".login");
+const reset = document.querySelector(".reset");
+const input1 = document.querySelector(".down .container .username")
+const input2 = document.querySelector(".down .container .password")
+
 
 
 login.addEventListener("click",() => {
     down.classList.remove("none")
+})
+
+reset.addEventListener("click",() => {
+    
+    input1.value = ""
+    input2.value = ""
 })
